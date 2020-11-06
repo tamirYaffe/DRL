2696ecede180903a520cfa2f0cd57bd594b3ef23
tamiryaffe <tamiryaffe144@gmail.com>
2020-11-06 18:24:50 +0200

    changed the algorithm to match requirements

--- QLearning.py
+++ QLearning.py
@@ -1,6 +1,5 @@
 from collections import defaultdict
 import numpy as np
-import itertools
 
 # Algorithm
 # 1. Create a lookup table containing the approximation of the Q-value for each state-action pair.
@@ -15,7 +14,7 @@ import itertools
 #           sample new initial state s'
 #       else:
 #           target = R(s, a, s') + ymaxQk(s', a')
-#       Qk+1(s, a) = (1- alpha) * Qk(s, a) + alpha * (target)
+#       Qk+1(s, a) = (1- 𝛼) * Qk(s, a) + 𝛼 * (target)
 #       s = s'
 
 
@@ -38,52 +37,72 @@ def epsilonGreedyAction(Q, epsilon, num_actions, state):
     return action
 
 
-def qLearning(env, num_episodes, discount_factor=1.0,
-              alpha=0.6, epsilon=0.1):
+def qLearning(env, num_episodes, discount_factor=0.95,
+              alpha=0.6, epsilon=0.3, decay_rate=0.95):
     """
     Q-Learning algorithm: Off-policy TD control.
     Finds the optimal greedy policy while improving
-    following an epsilon-greedy policy"""
+    following an epsilon-greedy policy.
 
-    # Action value function
-    # A nested dictionary that maps
-    # state -> (action -> action-value).
-    # todo: check if its the right dict to use
+    :param env: our environment, one of openai gym environments.
+    :param num_episodes: number of episodes we wish to learn from
+    :param discount_factor: use in order to reduce the value of future rewards.(should be 0< <1)
+    :param alpha: learning rate, Higher alpha means you are updating your Q values in big steps.(should decay over time)
+    :param epsilon: selecting random actions with epsilon probability.(should decay over time)
+    :param decay_rate: The decay rate for alpha and epsilon. (should be 0<<1)
+    :return: The learned Q-table and statistics about the learning process.
+    """
+
+    # 1. Create a lookup table containing the approximation of the Q-value for each state-action pair.
+    # 2. Initialize the table with zeros.
     Q = defaultdict(lambda: np.zeros(env.action_space.n))
+
+    # todo: check usage of decay rate.
+    # 3. Choose initial values for the hyper-parameters: learning rate 𝛼, discount
+    #    factor 𝛾, decay rate for decaying epsilon-greedy probability.
 
     # we need to keep track on.(Q-table,
     #       reward per episode, average number of steps to the goal over last 100 episodes)
     stats = {'episode_lengths': np.zeros(num_episodes),
              'episode_rewards': np.zeros(num_episodes)}
 
-    # For every episode
-    for episode in range(num_episodes):
+    # 4. Get initial state s
+    state = env.reset()
+    step_count = 0
+    episode = 1
 
-        # Reset the environment and pick the first action
-        state = env.reset()
+    # 5. For k = 1, 2, ... till convergence (5000 episodes)
+    while episode < 5000:
 
-        for t in itertools.count():
+        # As the learning goes on alpha and epsilon should decayed to stabilize and exploit the learned policy.
+        alpha *= decay_rate
+        epsilon *= decay_rate
 
-            # choose action according to epsilon greedy
-            action = epsilonGreedyAction(Q, epsilon, env.action_space.n, state)
+        # sample action a.(sample actions using decaying 𝜀 − 𝑔𝑟𝑒𝑒𝑑𝑦)
+        action = epsilonGreedyAction(Q, epsilon, env.action_space.n, state)
 
-            # take action and get reward, transit to next state
-            next_state, reward, done, _ = env.step(action)
+        # get next state s'.
+        # take action and get reward, transit to next state
+        next_state, reward, terminal, _ = env.step(action)
+        step_count += 1
 
-            # Update statistics
-            stats['episode_rewards'][episode] += reward
-            stats['episode_lengths'][episode] = t
+        # Update statistics
+        stats['episode_rewards'][episode] += reward
+        stats['episode_lengths'][episode] = step_count
 
-            # TD Update
+        # if s' is terminal: (max of 100 steps per episode)
+        if terminal or step_count == 100:
+            target = reward
+            # sample new initial state s'
+            state = env.reset()
+            step_count = 0
+            episode += 1
+
+        else:
             best_next_action = np.argmax(Q[next_state])
-            td_target = reward + discount_factor * Q[next_state][best_next_action]
-            td_delta = td_target - Q[state][action]
-            Q[state][action] += alpha * td_delta
+            target = reward + discount_factor * Q[next_state][best_next_action]
 
-            # done is True if episode terminated
-            if done:
-                break
-
-            state = next_state
+        Q[state][action] = (1-alpha) * Q[state][action] + alpha * target
+        state = next_state
 
     return Q, stats
